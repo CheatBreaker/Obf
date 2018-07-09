0eb20fde8fe4643bcd642efd65064c4ca9c5bbf9
Michael Himing <mhiming@gmail.com>
2018-07-09 20:58:23 +1000

    Store strings in multiple classes (1024 per class)

--- src/main/java/com/cheatbreaker/obf/transformer/StringTransformer.java
+++ src/main/java/com/cheatbreaker/obf/transformer/StringTransformer.java
@@ -13,17 +13,15 @@ import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.TypeInsnNode;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 public class StringTransformer extends Transformer {
 
-    int currentId = 0;
-    private Map<Integer, String> idToString = new HashMap<>();
-    private Map<String, Integer> stringToId = new HashMap<>();
+    private static final int PARTITION_BITS = 10;
+    private static final int PARTITION_SIZE = 1 << PARTITION_BITS;
+    private static final int PARTITION_MASK = PARTITION_SIZE - 1;
+    private List<String> strings = new ArrayList<>();
 
     public StringTransformer(Obf obf) {
         super(obf);
@@ -38,18 +36,17 @@ public class StringTransformer extends Transformer {
                     LdcInsnNode ldc = (LdcInsnNode) insn;
                     if (ldc.cst instanceof String) {
                         String string = (String) ldc.cst;
-                        int id;
-                        if (stringToId.containsKey(string)) {
-                            id = stringToId.get(string);
-                        } else {
-                            id = currentId++;
-                            idToString.put(id, string);
-                            stringToId.put(string, id);
+                        int id = strings.indexOf(string);
+                        if (id == -1) {
+                            id = strings.size();
+                            strings.add(string);
                         }
-                        int mask = random.nextInt();
-                        int a = random.nextInt() & mask | id;
-                        int b = random.nextInt() & ~mask | id;
-                        method.instructions.insertBefore(insn, new FieldInsnNode(Opcodes.GETSTATIC, "generated/Strings", "strings", "[Ljava/lang/String;"));
+                        int index = id & PARTITION_MASK;
+                        int classId = id >> PARTITION_BITS;
+                        int mask = (short) random.nextInt();
+                        int a = (short) random.nextInt() & mask | index;
+                        int b = (short) random.nextInt() & ~mask | index;
+                        method.instructions.insertBefore(insn, new FieldInsnNode(Opcodes.GETSTATIC, "generated/Strings" + classId, "strings", "[Ljava/lang/String;"));
                         method.instructions.insertBefore(insn, AsmUtils.pushInt(a));
                         method.instructions.insertBefore(insn, AsmUtils.pushInt(b));
                         method.instructions.insertBefore(insn, new InsnNode(Opcodes.IAND));
@@ -63,27 +60,28 @@ public class StringTransformer extends Transformer {
 
     @Override
     public void after() {
-        ClassNode classNode = new ClassNode();
-        classNode.version = Opcodes.V1_8;
-        classNode.access = Opcodes.ACC_PUBLIC;
-        classNode.name = "generated/Strings";
-        classNode.superName = "java/lang/Object";
-        FieldNode strings = new FieldNode(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "strings", "[Ljava/lang/String;", null, null);
-        classNode.fields.add(strings);
-        MethodNode clinit = new MethodNode(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
-        classNode.methods.add(clinit);
-        List<Map.Entry<Integer, String>> entries = new ArrayList<>(idToString.entrySet());
-        Collections.shuffle(entries, random);
-        clinit.instructions.add(AsmUtils.pushInt(entries.size()));
-        clinit.instructions.add(new TypeInsnNode(Opcodes.ANEWARRAY, "Ljava/lang/String;"));
-        for (Map.Entry<Integer, String> entry : entries) {
-            clinit.instructions.add(new InsnNode(Opcodes.DUP));
-            clinit.instructions.add(AsmUtils.pushInt(entry.getKey()));
-            clinit.instructions.add(new LdcInsnNode(entry.getValue()));
-            clinit.instructions.add(new InsnNode(Opcodes.AASTORE));
+        for (int classId = 0; classId <= strings.size() >> PARTITION_BITS; classId++) {
+            ClassNode classNode = new ClassNode();
+            classNode.version = Opcodes.V1_8;
+            classNode.access = Opcodes.ACC_PUBLIC;
+            classNode.name = "generated/Strings" + classId;
+            classNode.superName = "java/lang/Object";
+            classNode.fields.add(new FieldNode(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "strings", "[Ljava/lang/String;", null, null));
+            MethodNode clinit = new MethodNode(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
+            classNode.methods.add(clinit);
+            int start = classId << PARTITION_BITS;
+            int end = Math.min(start + PARTITION_SIZE, strings.size());
+            clinit.instructions.add(AsmUtils.pushInt(end - start));
+            clinit.instructions.add(new TypeInsnNode(Opcodes.ANEWARRAY, "Ljava/lang/String;"));
+            for (int id = start; id < end; id++) {
+                clinit.instructions.add(new InsnNode(Opcodes.DUP));
+                clinit.instructions.add(AsmUtils.pushInt(id & PARTITION_MASK));
+                clinit.instructions.add(new LdcInsnNode(strings.get(id)));
+                clinit.instructions.add(new InsnNode(Opcodes.AASTORE));
+            }
+            clinit.instructions.add(new FieldInsnNode(Opcodes.PUTSTATIC, classNode.name, "strings", "[Ljava/lang/String;"));
+            clinit.instructions.add(new InsnNode(Opcodes.RETURN));
+            obf.addNewClass(classNode);
         }
-        clinit.instructions.add(new FieldInsnNode(Opcodes.PUTSTATIC, "generated/Strings", "strings", "[Ljava/lang/String;"));
-        clinit.instructions.add(new InsnNode(Opcodes.RETURN));
-        obf.addNewClass(classNode);
     }
 }
